b59b2a91a94b89d2c837aa16d6d493b19dd7b532
Elizabeth <ekorj@student.fitchburgstate.edu>
2024-12-03 12:13:40 -0500

    barely got anything done oof

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -16,24 +16,27 @@ public class GameManager : MonoBehaviour
     public float coinTime;
     public GameObject coin;
     public bool isGameActive;
-    private float spawnRate = 30.0f;
+    private float spawnRate = 10.0f;
     private float minSpeed = 12;
     private float maxSpeed = 16;
-    private float maxTorque = 10;
-    private float xRange = 250;
-    private float ySpawnPos = 50;
+    private float maxTorque = 50;
+    private float xRange = 90;
+    private float ySpawnPos = -20;
+    // private float zSpawnPos = -90;
     private GameManager gameManager;
+    public float destroyTime = 5.0f;
     void Start()
     {
         coins = 0;
         UpdateCoins(0);
         StartCoroutine(CoinPassive());
         StartCoroutine(SpawnCoins());
+        // StartCoroutine(DeleteCoins());
 
-        targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
-        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
-        transform.position = RandomSpawnPos();
+       // targetRb = GetComponent<Rigidbody>();
+       // targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+       // targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+       // transform.position = RandomSpawnPos();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
@@ -53,36 +56,48 @@ public class GameManager : MonoBehaviour
             UpdateCoins(1);
         }
     }
-Vector3 RandomForce()
-{
-    return Vector3.up * Random.Range(minSpeed, maxSpeed);
-}
-float RandomTorque()
-{
-    return Random.Range(-maxTorque, maxTorque);
-}
-Vector3 RandomSpawnPos()
-{
-    return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
-}
 
-IEnumerator SpawnCoins()
+    // shoot coins up for player to click on
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    IEnumerator SpawnCoins()
     {
         while (true)
         {
             yield return new WaitForSeconds(spawnRate);
 
-            Instantiate(coin);
+            GameObject c = Instantiate(coin, RandomSpawnPos(), coin.transform.rotation);
+            Rigidbody rb = c.GetComponent<Rigidbody>();
+            rb.AddForce(RandomForce(), ForceMode.Impulse);
+            rb.AddTorque(new Vector3 (RandomTorque(), RandomTorque(), RandomTorque()));
             OnMouseDown();
         }
 
-    }
+    } 
+
+    // IEnumerator DeleteCoins()
+   // {
+        
+      
+  //  }
 
     private void OnMouseDown()
     {
         if (isGameActive)
         {
             coins+= 30;
+            Destroy(coin);
         }
     }
 
