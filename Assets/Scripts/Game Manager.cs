ebed0d81332ea896822c25f454c151e93871174e
Elizabeth <ekorj@student.fitchburgstate.edu>
2024-12-10 01:48:03 -0500

    why

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -15,19 +15,25 @@ public class GameManager : MonoBehaviour
     private Rigidbody targetRb;
     public TextMeshProUGUI coinsCounter;
     public TextMeshProUGUI btnText;
+    public TextMeshProUGUI superbtnText;
     // This is the slider object, do not use the TextMeshPro object, they are different.
     public UnityEngine.UI.Slider healthSlider;
-    public int coins;
+    public float coins;
     public float coinTime;
     public GameObject coin;
-    public bool isGameActive;
+    public bool isGameActive = false;
     private float spawnRate = 5.0f;
     private float zSpawnPos = -10;
     private GameManager gameManager;
     private float xRange = 90;
     private float ySpawnPos = -30;
-    private int upgradeCost;
-    private int damage = 5;
+    private float upgradeCost;
+    private int superhitCost;
+    private float coinPerSecond = 1;
+    private float count = 0;
+    public float damageTime = 3;
+    public GameObject Enemy;
+    public float enemySpawnRate = 1.0f;
 
     void Start()
     {
@@ -37,6 +43,8 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnCoins());
         isGameActive = true;
         upgradeCost = 5;
+        superhitCost = 50;
+        healthSlider.AddComponent<TextMeshProUGUI>();
         // Example of a call to your slider. Slider name . call (on a value change from some outside source) . AddListener (which is given a method as a parameter)
         // Example: AddListener(DecreaseHealth()); with DecreaseHealth being a public void? (maybe int), that decreases a count.;
         // healthSlider.onValueChanged.AddListener();
@@ -44,9 +52,10 @@ public class GameManager : MonoBehaviour
 
     // coin related things
 
-    public void UpdateCoins(int scoreToAdd)
+    public void UpdateCoins(float scoreToAdd)
     {
-        coins++;
+        coins += scoreToAdd;
+        coins = UnityEngine.Mathf.Round(coins);
         coinsCounter.text = "Coins: " + coins;
     }
 
@@ -55,11 +64,11 @@ public class GameManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(coinTime);
-            UpdateCoins(1);
+            UpdateCoins(coinPerSecond);
         }
     }
 
-    // shoot coins up for player to click on
+    // spawns coins
 
     Vector3 RandomSpawnPos()
     {
@@ -76,15 +85,75 @@ public class GameManager : MonoBehaviour
         }
 
     } 
-
+    // upgrade button
     public void Upgrade() 
     {
-        if (coins > upgradeCost)
+        if (coins >= upgradeCost)
         {
+            count++;
             coins -= upgradeCost;
-            upgradeCost *= 2;
+            upgradeCost *= Mathf.Pow(2f, (count/(2+count)));
+            upgradeCost = UnityEngine.Mathf.Round(upgradeCost);
             btnText.text = ( "Upgrade Cost: " + upgradeCost);
-            damage += upgradeCost;
+            coinPerSecond += 1.2f+(coinPerSecond/(count/2));
         }
+    }
+    // superhit cost
+    public void SuperHit()
+    {
+        if (coins > superhitCost)
+        {
+            coins -= superhitCost;
+            superhitCost *= 2;
+            superbtnText.text = ("Superhit Cost: " +  superhitCost);
+            coinPerSecond += superhitCost;
+        }
+    }
+
+    // slider health, damage, and enemy spawning
+    //public void DecreaseHealth()
+    //{
+    //    while ((isGameActive == true) & (Enemy.gameObject == true))
+    //    {
+    //        if (healthSlider.value <= 0)
+    //        {
+
+    //            Destroy(healthSlider);
+    //            Destroy(Enemy.gameObject);
+    //            Respawn();
+
+    //        }
+    //        healthSlider.value -= damage;
+    //        Damage();
+    //    }
+    //}
+    //IEnumerator Damage()
+    //{
+    //    while (true) 
+    //    {
+    //        yield return new WaitForSeconds(damageTime);
+    //    } 
+    //}
+
+    //IEnumerator Respawn()
+    //{
+    //    while (true)
+    //    {
+    //        yield return new WaitForSeconds(enemySpawnRate);
+    //        RespawnEnemy();
+    //    }
+    //}
+
+    //public void RespawnEnemy()
+    //{
+    //    healthSlider.value = healthSlider.value * damage;
+    //    Instantiate(Enemy);
+    //    Instantiate(healthSlider);
+    //}
+
+    void Update()
+    {
+        //StartCoroutine(Damage());
+
     }
 }
